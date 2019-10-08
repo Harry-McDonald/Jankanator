3399ce208be048b137c7dc287b95cc3ff4e6ea42
Finn Wedge <f.wedge@uq.net.au>
2019-10-08 13:50:16 +1000

    Hello World

---
+++ test2.py
@@ -0,0 +1,12 @@
+#---------------Funtions-----------------
+#rpm to rad/s converter 
+def rpm2rad(rpm):
+  rads = rpm*2*np.pi/60
+  return np.round(rads,dec)
+
+print('Hello World')
+
+def findRow(rowName):
+  for row in range(num_rows):
+    if row_names[row] == rowName:
+      return str(row+1)
